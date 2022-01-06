98351d2ecd12095b6a6ec1913e6306a1ef66f722
blfernandezGit <blfernandez@up.edu.ph>
2022-01-06 15:04:10 +0800

    add ticket title and set status default

--- db/migrate/20220106064942_create_tickets.rb
+++ db/migrate/20220106064942_create_tickets.rb
@@ -1,9 +1,10 @@
 class CreateTickets < ActiveRecord::Migration[6.1]
   def change
     create_table :tickets do |t|
+      t.string :title, null: false
       t.text :description
       t.text :resolution
-      t.string :status
+      t.string :status, null: false, default: 'open'
       t.belongs_to :project, null: false, foreign_key: true
       t.references :author_id, null: false
       t.references :assignee_id
