180b763421cbc8cb4b5e7d783cf9f2bf6c79db88
blfernandezGit <blfernandez@up.edu.ph>
2022-01-06 17:57:54 +0800

    fix project model association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
     # Projecect belongs to and has many users
     has_many :project_memberships
-    has_many :user, though: :project_memberships
+    has_many :users, through: :project_memberships
 end
