85f1ea721e7fa6aaa94ff3586c852529d3085de1
blfernandezGit <blfernandez@up.edu.ph>
2022-01-06 17:57:30 +0800

    modify configs to include in rails_helper.rb instead

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association Tests
+  it { should have_many(:project_memberships)}
+  it { should have_many(:projects)}
+  it { should have_many(:author_tickets)}
+  it { should have_many(:assignee_tickets)}
+  it { should have_many(:comments)}
+
+  # Validation Tests
+  it { is_expected.to validate_presence_of(:first_name)}
 end
