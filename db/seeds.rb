271189b76a2f7748f75800b9e10e207c68a75bd4
blfernandezGit <blfernandez@up.edu.ph>
2022-01-11 21:40:28 +0800

    add temporary seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Project.create([
+#     { name: 'Project 1' },
+#     { name: 'Project 2' }
+# ])
+
+# User.create([
+#     {
+#         first_name: 'John',
+#         last_name: 'Cruz',
+#         email: 'johncruz@sharklasers.com',
+#         username: 'johncruz',
+#         password: 'P@ssw0rd'
+#     }
+# ])
+
+# ProjectMembership.create([
+#     { 
+#         project_id: Project.first.id,
+#         user_id: User.first.id
+#     }
+# ])
