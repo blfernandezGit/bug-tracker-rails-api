41e175e33d94bfbcdfdbb2522e6666732407c27b
blfernandezGit <blfernandez@up.edu.ph>
2022-01-11 21:37:43 +0800

    add root and project controller

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ProjectsController < Api::V1::RootController
+    def index
+        projects = Project.all
+
+        render json: ProjectSerializer.new(projects).serializable_hash.to_json
+    end
+
+    def show
+        project = Project.find_by(code: params[:code])
+        render json: ProjectSerializer.new(project).serializable_hash.to_json
+    end
+end
+
