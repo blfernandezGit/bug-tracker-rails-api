e6698403bf5a45e6f0794a5742da69a708f41bc0
blfernandezGit <blfernandez@up.edu.ph>
2022-01-12 19:22:51 +0800

    method using jwt to encode token using user id

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true, uniqueness: true
+
+  def generate_jwt
+    JWT.encode({id: id, exp: 60.days.from_now.to_i}, Rails.application.secrets.secret_key_base)
+  end
 end
