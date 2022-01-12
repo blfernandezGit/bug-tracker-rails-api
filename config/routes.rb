a97d3f5cd0aa46c0ea64d537ba1e6c676d981ba8
blfernandezGit <blfernandez@up.edu.ph>
2022-01-12 19:23:19 +0800

    overwrite default devise functionality (registration, session)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: {
+      registrations: :registrations,
+      sessions: :sessions
+    }
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :projects, param: :code, only: %w[index show] #use code-slug as primary parameter instead of id for projects routing
