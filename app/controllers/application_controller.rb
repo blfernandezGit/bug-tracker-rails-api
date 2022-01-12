7aa877e057428a3a614ea75d3638ea0956c1b05f
blfernandezGit <blfernandez@up.edu.ph>
2022-01-12 19:34:29 +0800

    add jwt handling with overridden devise methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+    respond_to :json
+
+    before_action :process_token
+
+    # Override devise methods
+    def authenticate_user!(options = {})
+        head :unauthorized unless signed_in?
+    end
+
+    def signed_in?
+        @current_user_id.present?
+    end
+
+    def current_user
+        @current_user ||= super || User.find(@current_user_id)
+    end
+
+    # Extract user id from passed jwt using jwt gem with error handling
+    def process_token
+        if request.headers['Authorization'].present?
+            begin
+                jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1].remove('"'), Rails.application.secrets.secret_key_base).first
+                @current_user_id = jwt_payload['id']
+            rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+                head :unauthorized
+            end
+        end
+    end
 end
