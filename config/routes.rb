ed7695b87fd4e136423769b009cc859590227b6e
blfernandezGit <blfernandez@up.edu.ph>
2022-01-12 19:18:48 +0800

    add devise user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :projects, param: :code, only: %w[index show] #use code-slug as primary parameter instead of id for projects routing
