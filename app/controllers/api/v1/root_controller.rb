7fec9eaa770a9a08417e09d83be9f7ce68d051e6
blfernandezGit <blfernandez@up.edu.ph>
2022-01-13 20:15:24 +0800

    move token processing to root controller

--- app/controllers/api/v1/root_controller.rb
+++ app/controllers/api/v1/root_controller.rb
@@ -1,15 +1,43 @@
 class Api::V1::RootController < ApplicationController
-    # before_action :authenticate_user!
+    before_action :process_token
+    before_action :authenticate_api_v1_user!
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    # Extract user id from passed jwt using jwt gem with error handling
+    def process_token
+        if request.headers['Authorization'].present?
+            begin
+                jwt_payload = JWT.decode(request.headers['Authorization'], Rails.application.secrets.secret_key_base).first
+                @current_user_id = jwt_payload['id']
+            rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+                unauthorized
+            end
+        else
+            unauthorized
+        end
+    end
+
+    def unauthorized
+        render json: { 
+            status: "401", 
+            errors: [
+                {
+                    title: "Unauthorized",
+                    messages: [ "You need to sign in or sign up before continuing." ]
+                }
+            ]
+        }, status: :unauthorized
+    end
 
     def not_found
         render json: {
-            'errors': [
+            status: "404",
+            errors: [
                 {
-                    'status': '404',
-                    'title': 'Not Found'
+                    title: "Not Found",
+                    messages: [ "Page not found." ]
                 }
             ]
-        }, status: 404
+        }, status: :not_found
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::API
     respond_to :json
 
-    before_action :process_token
-
     # Override devise methods
     def authenticate_api_v1_user!(options = {})
         head :unauthorized unless signed_in?
@@ -14,26 +12,5 @@ class ApplicationController < ActionController::API
 
     def current_user
         @current_user ||= super || User.find(@current_user_id)
-    end
-
-    # Extract user id from passed jwt using jwt gem with error handling
-    def process_token
-        if request.headers['Authorization'].present?
-            begin
-                jwt_payload = JWT.decode(request.headers['Authorization'], Rails.application.secrets.secret_key_base).first
-                @current_user_id = jwt_payload['id']
-            rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
-                unauthorized
-            end
-        end
-    end
-
-    private
-
-    def unauthorized
-        render json: { 
-            status: "401", 
-            errors: {message:"You need to sign in or sign up before continuing." }
-        }, status: :unauthorized
     end
 end
