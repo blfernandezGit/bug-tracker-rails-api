9316aa4553264614e682d4c2ea9173131aa9e41c
blfernandezGit <blfernandez@up.edu.ph>
2022-01-13 17:44:35 +0800

    change token expiration

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   def generate_jwt
-    JWT.encode({id: id, exp: 60.days.from_now.to_i}, Rails.application.secrets.secret_key_base)
+    JWT.encode({id: id, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
   end
 end
