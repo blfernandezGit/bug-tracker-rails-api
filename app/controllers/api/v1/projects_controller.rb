1eb796505fce0290c1487973b43ad22e5215a4d9
blfernandezGit <blfernandez@up.edu.ph>
2022-01-14 01:53:27 +0800

    add project methods

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,12 +1,80 @@
 class Api::V1::ProjectsController < Api::V1::RootController
+    before_action :authenticate_api_v1_admin!, only: [:create, :update, :destroy]
+    before_action :set_project, only: [:update, :destroy]
   def index
     projects = Project.all
-
     render json: ProjectSerializer.new(projects).serializable_hash.to_json
   end
 
   def show
     project = Project.find_by(code: params[:code])
     render json: ProjectSerializer.new(project).serializable_hash.to_json
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+        render json: {
+            status: '200',
+            data: ProjectSerializer.new(@project).serializable_hash,
+            messages: [ 'Project successfully created.' ]
+        }, status: :ok
+    else
+        render json: {
+            status: '422',
+            errors: [
+                title: 'Unprocessable Entity',
+                messages: @project.errors.full_messages
+            ]    
+        }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+        render json: {
+            status: '200',
+            data: ProjectSerializer.new(@project).serializable_hash,
+            messages: [ 'Project successfully updated.' ]
+        }, status: :ok
+      else
+        render json: {
+            status: '422',
+            errors: [
+                title: 'Unprocessable Entity',
+                messages: @project.errors.full_messages
+            ]    
+        }, status: :unprocessable_entity
+      end
+  end
+
+  def destroy
+    if @project.destroy
+      render json: {
+        status: '200',
+        deletedData: ProjectSerializer.new(@project).serializable_hash,
+        messages: [ 'Project successfully deleted.' ]
+      }, status: :ok
+    else
+      render json: {
+        status: '422',
+        errors: [
+            title: 'Unprocessable Entity',
+            messages: @project.errors.full_messages
+        ]    
+      }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find_by(code: params[:code])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def project_params
+    params.permit(:name, :code, :description, :is_active)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   # Projecect belongs to and has many users
-  has_many :project_memberships
+  has_many :project_memberships, dependent: :destroy
   has_many :users, through: :project_memberships
   # has_many :tickets
 
