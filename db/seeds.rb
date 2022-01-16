4161884d17b93a2c2d3fd24e35fd513f9a61d5f3
blfernandezGit <blfernandez@up.edu.ph>
2022-01-16 17:14:27 +0800

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,28 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-# Project.create([
-#     { name: 'Project 1' },
-#     { name: 'Project 2' }
-# ])
+User.create([
+    {
+        first_name: 'Support',
+        last_name: 'Administrator',
+        email: 'suppadmin@sharklasers.com',
+        username: 'suppadmin',
+        password: 'n@b!dEMo818#)',
+        is_admin: true
+    }
+])
 
-# User.create([
-#     {
-#         first_name: 'John',
-#         last_name: 'Cruz',
-#         email: 'johncruz@sharklasers.com',
-#         username: 'johncruz',
-#         password: 'P@ssw0rd'
-#     }
-# ])
+Project.create([
+    { 
+        name: 'Nabi Project',
+        description: 'React + Ruby on Rails Bug Tracking Application',
+        code: 'nabi-project'
+    }
+])
 
-# ProjectMembership.create([
-#     { 
-#         project_id: Project.first.id,
-#         user_id: User.first.id
-#     }
-# ])
+ProjectMembership.create([
+    { 
+        project_id: Project.first.id,
+        user_id: User.first.id
+    }
+])
