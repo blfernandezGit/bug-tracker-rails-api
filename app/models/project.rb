dc5cc8ecb88602fe55f9ac02a7d05e74eb180a47
blfernandezGit <blfernandez@up.edu.ph>
2022-01-23 11:05:56 +0800

    add ticket dependency on destroy project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   # Projecect belongs to and has many users
   has_many :project_memberships, dependent: :destroy
   has_many :users, through: :project_memberships
-  has_many :tickets
+  has_many :tickets, dependent: :destroy
 
   validates :name, presence: true
   validates :code, uniqueness: true
